4082c8fe6e54bcbf93045ddb265073f529bd17fa
Bruno Samir Bocanegra C. <elmariachi1bb1s@gmail.com>
2025-11-24 10:20:03 -0500

    .

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,11 @@ use App\Http\Controllers\PublicacionController;
 use App\Http\Controllers\ComunicadoController;
 use App\Http\Controllers\EventoController;
 use App\Http\Controllers\OrganizacionCardController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\DocumentacionController;
-=======
+
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\AdminController;
->>>>>>> a335ba13581e95dadc75897fe0658c43e245a5b6
+
 
 Route::get('/', [PageController::class, 'welcome'])->name('welcome');
 
@@ -33,7 +32,7 @@ Route::get('/arbitral', [PageController::class, 'arbitral'])->name('arbitral');
 Route::get('/junta-res-disputas', [PageController::class, 'juntaResDisputas'])->name('junta-res-disputas');
 Route::get('/dispute-review', [PageController::class, 'disputeReview'])->name('dispute-review');
 Route::get('/dispute-avoidance-res', [PageController::class, 'disputeAvoidanceRes'])->name('dispute-avoidance-res');
-<<<<<<< HEAD
+
 Route::get('/convocatoria', [PageController::class, 'convocatoria'])->name('convocatoria');
 
 Route::get('/calculadora/institucion/determinada', [PageController::class, 'calcInstDeterminada'])->name('calc.inst.det');
@@ -41,15 +40,7 @@ Route::get('/calculadora/institucion/indeterminada', [PageController::class, 'ca
 Route::get('/calculadora/junta/determinada', [PageController::class, 'calcJuntaDeterminada'])->name('calc.junta.det');
 Route::get('/calculadora/junta/indeterminada', [PageController::class, 'calcJuntaIndeterminada'])->name('calc.junta.indet');
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-=======
-
-
-// aca entra el contenido de proteccion de rutas
 Route::middleware(['auth', 'checkrole:gestor_contenido'])->group(function () {
-
->>>>>>> a335ba13581e95dadc75897fe0658c43e245a5b6
 Route::get('/gestion-contenido', [PageController::class, 'gestionContenido'])->name('gestion-contenido');
 Route::resource('publicaciones', PublicacionController::class);
 Route::put('/gestor/publicaciones/{id}/estado', [PublicacionController::class, 'toggleEstado'])
@@ -64,10 +55,9 @@ Route::resource('gestor/eventos', EventoController::class)->names('eventos');
 Route::put('/gestor/eventos/{id}/estado', [EventoController::class, 'toggleEstado'])->name('eventos.toggle');
 Route::resource('gestor/organizacion', OrganizacionCardController::class)->names('organizacion-gestion');
 Route::put('/gestor/organizacion/{id}/estado', [OrganizacionCardController::class, 'toggleEstado'])->name('organizacion-gestion.toggle');
-<<<<<<< HEAD
+
 Route::resource('gestor/documentos', DocumentacionController::class)->names('documentos-gestion');
 Route::put('/gestor/documentos/{id}/estado', [DocumentacionController::class, 'toggleEstado'])->name('documentos-gestion.toggle');
-=======
 
 });
 
@@ -89,6 +79,5 @@ Route::get('/admin/dashboard', function () {
 })->middleware(['auth','checkrole:admin'])
   ->name('Admin/dashboard');
 
-
 require __DIR__.'/auth.php';
->>>>>>> a335ba13581e95dadc75897fe0658c43e245a5b6
+
